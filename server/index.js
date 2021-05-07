ab9066385647c265675197bd7c2d62c467dee5f7
Daniel Deverell <daniel.deverell@gmail.com>
2021-05-06 20:49:42 -0400

    moved server

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const path = require("path");
 
 const app = express();
 
-app.use("/dist", static(path.join(__dirname, "dist")));
+app.use("/dist", static(path.join(__dirname, "../dist")));
 
 app.get("/", (req, res, next) =>
-  res.sendFile(path.join(__dirname, "index.html"))
+  res.sendFile(path.join(__dirname, "../index.html"))
 );
 
 app.get("/api/users", async (req, res, next) => {

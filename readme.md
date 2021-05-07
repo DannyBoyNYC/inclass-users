b3a732063d976ccbfba542b1ecc2f9f670601d79
Daniel Deverell <daniel.deverell@gmail.com>
2021-05-06 20:19:29 -0400

    server.js

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Users
 
-```js
+```sh
 docker run --name my-postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d --rm postgres:13.0
 docker exec -it -u postgres my-postgres psql
 ```
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const { static } = express;
+const path = require("path");
+
+const app = express();
+
+app.use("/dist", static(path.join(__dirname, "dist")));
+
+app.get("/", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "index.html"))
+);
+
+app.get("/api/users", async (req, res, next) => {
+  try {
+    res.send(await userInfo.findAll());
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+const init = async () => {
+  try {
+    await syncAndSeed();
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`listening on ${port}`));
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+const Sequelize = require("sequelize");
+const { STRING } = Sequelize;
+const conn = new Sequelize(
+  "postgres://postgres:mysecretpassword@localhost:5432/acme_db"
+);
+
+const User = conn.define("user", {
+  name: STRING,
+});
+
+const syncAndSeed = async () => {
+  await conn.sync({ force: true });
+  await Promise.all([
+    User.create({ name: "Moe" }),
+    User.create({ name: "Larry" }),
+    User.create({ name: "Curly" }),
+  ]);
+};
+
+init();

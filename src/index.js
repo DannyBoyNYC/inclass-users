10b4822f2a954e1d18cdd6e9643979044c8c31a2
Daniel Deverell <daniel.deverell@gmail.com>
2021-05-06 21:04:34 -0400

    webpack loaders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 
+import "./styles.css";
+
 const App = () => {
   const [users, setUsers] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,25 @@ module.exports = {
           presets: ["@babel/preset-react"],
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
 };
